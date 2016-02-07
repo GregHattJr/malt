2345cd2138ffdd5b453454f83b223de2ea375751
Darren Mulholland <dmulholland@outlook.ie>
2016-02-07 19:51:07 +0000

    Add compact version to the report plugin

--- ark/ini/ext/report.py
+++ ark/ini/ext/report.py
@@ -8,6 +8,10 @@
 from ark import hooks, site
 
 
+# Plugin version number.
+__version__ = "1.0.0"
+
+
 # Register a callback on the 'exit' event hook.
 @hooks.register('exit')
 def print_status_report():
@@ -17,11 +21,27 @@ def print_status_report():
     if num_rendered == 0:
         return
 
+    # Singluar or plural?
     txt_rendered = "1 page" if num_rendered == 1 else "%s pages" % num_rendered
     txt_written = "1 page" if num_written == 1 else "%s pages" % num_written
 
+    # The runtime() function gives the application's runtime in seconds.
     time = site.runtime()
     average = time / num_rendered
 
-    status = "%s rendered, %s written in %.2f seconds. %.4f seconds per page."
-    print(status % (txt_rendered, txt_written, time, average))
+    # Default status report.
+    f = "%s rendered, %s written in %.2f seconds. %.4f seconds per page."
+    default = f % (txt_rendered, txt_written, time, average)
+
+    # Compact report.
+    f = "Rendered: %5d  |  Written: %5d  |  Time: %5.2f sec  |  Avg: %.4f sec/pg"
+    compact = f % (num_rendered, num_written, time, average)
+
+    # Print the compact version if the default is too long or if the 'watch'
+    # command is running.
+    if len(default) > 80:
+        print(compact)
+    elif 'watching' in site.flags() and not 'firstwatch' in site.flags():
+        print(compact)
+    else:
+        print(default)
