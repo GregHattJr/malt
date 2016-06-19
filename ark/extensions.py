1c469576191c8fa894534901442f29a182d1b85c
Darren Mulholland <darren@mulholland.xyz>
2016-06-19 10:18:31 +0100

    Dev-friendly plugin loading

--- ark/extensions.py
+++ ark/extensions.py
@@ -15,27 +15,58 @@ _loaded = {}
 
 # Loads any Python modules found in the extensions directories.
 def load():
-    dirs = []
+
+    # List of directories to search for extensions.
+    extdirs = []
 
     # Default extensions bundled with Ark.
-    dirs.append(os.path.join(os.path.dirname(__file__), 'ext'))
+    extdirs.append(os.path.join(os.path.dirname(__file__), 'ext'))
 
-    # Global extensions directory.
-    if os.getenv('ARK_EXT') and os.path.isdir(os.getenv('ARK_EXT')):
-        dirs.append(os.getenv('ARK_EXT'))
+    # Global extensions directory/directories.
+    if os.getenv('ARK_EXT'):
+        for path in os.getenv('ARK_EXT').split(os.pathsep):
+            if os.path.isdir(path):
+                extdirs.append(path)
 
     # Site-specific extensions.
     if os.path.isdir(site.ext()):
-        dirs.append(site.ext())
+        extdirs.append(site.ext())
 
     # Load extensions.
-    for dirpath in dirs:
-        sys.path.insert(0, dirpath)
-        for name in os.listdir(dirpath):
-            base = os.path.splitext(name)[0]
-            if not base[0] in '_.':
-                _loaded[base] = importlib.import_module(base)
-        sys.path.pop(0)
+    for extdir in extdirs:
+        for item in os.listdir(extdir):
+            itembase = os.path.splitext(item)[0]
+            itempath = os.path.join(extdir, item)
+
+            # Skip garbage.
+            if item[0] in '_.':
+                continue
+
+            # Is the item a directory?
+            if os.path.isdir(itempath):
+
+                # Is the item a directory containing an extension directory?
+                if os.path.isdir(os.path.join(itempath, item)):
+                    _load(itempath, item)
+
+                # Is the item a directory containing an extension file?
+                elif os.path.isfile(os.path.join(itempath, item + '.py')):
+                    _load(itempath, item)
+
+                # Else, assume the item directory *is* the extension.
+                else:
+                    _load(extdir, item)
+
+            # The item is a file. Assume it's a Python module.
+            else:
+                _load(extdir, itembase)
+
+
+# Load the named module from the specified directory.
+def _load(directory, module):
+    sys.path.insert(0, directory)
+    _loaded[module] = importlib.import_module(module)
+    sys.path.pop(0)
 
 
 # Returns the dictionary of loaded extension modules.
