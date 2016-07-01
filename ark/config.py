a17682ef6246dce4dafb79586df558a1c61e1b42
Darren Mulholland <darren@mulholland.xyz>
2016-07-01 18:37:34 +0100

    Set phoenix as the default theme

--- ark/config.py
+++ ark/config.py
@@ -12,7 +12,7 @@
 
 # Name of the default theme directory.
 
-theme = "twentyfifteen"
+theme = "phoenix"
 
 # --------------------------------------------------------------------------
 # Root URL
--- ark/ini/config.py
+++ ark/ini/config.py
@@ -15,7 +15,7 @@
 
 # The name of the active theme directory in your site's 'lib' folder.
 
-theme = "twentyfifteen"
+theme = "phoenix"
 
 # --------------------------------------------------------------------------
 # Root URL
