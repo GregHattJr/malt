5d70efe6b19b6eaab6cfe7014c59b6faea5989e5
Darren Mulholland <darren@mulholland.xyz>
2016-07-01 19:18:48 +0100

    Update skeleton site for phoenix theme

--- ark/ini/config.py
+++ ark/ini/config.py
@@ -6,25 +6,30 @@
 # customise how Ark builds your site. Variables you set here are also
 # available to themes and plugins.
 
-# You can safely delete this file if you don't need to change any of
-# the default settings.
-
 # --------------------------------------------------------------------------
-# Theme Directory
+# Theme Settings
 # --------------------------------------------------------------------------
 
-# The name of the active theme directory in your site's 'lib' folder.
-
+# The theme to use when building the site. This attribute should specify
+# the name of a theme directory in the site's 'lib' folder.
 theme = "phoenix"
+
+# The default theme displays the 'title' string as the site's title.
+title = "Site Title"
+
+# The default theme displays the 'tagline' string below the site's title.
+tagline = "Just another Ark site"
+
+# The default theme displays the 'copyright' string in the site's footer.
+copyright = "Copyright &copy; 2016 Foobar Industries"
 
 # --------------------------------------------------------------------------
 # Root URL
 # --------------------------------------------------------------------------
 
-# Your root url can be an explicit domain ("http://example.com/) for
+# Your root url can be an explicit domain ("http://example.com/") for
 # absolute urls, a single forward slash ("/") for site-relative urls, or an
 # empty string (the default) for page-relative urls.
-
 root = ""
 
 # --------------------------------------------------------------------------
@@ -34,5 +39,4 @@ root = ""
 # You can choose an arbitrary file extension for generated files (".html")
 # or pass an empty string ("") to use no extension at all. Specify a single
 # forward slash ("/") to generate directory-style urls.
-
 extension = ".html"
