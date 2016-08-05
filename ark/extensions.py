60585919858cb5f83172b270af7f5ba9028c117b
Darren Mulholland <darren@mulholland.xyz>
2016-08-05 10:24:45 +0100

    Remove 'dev-friendly' plugin loading

--- ark/extensions.py
+++ ark/extensions.py
@@ -11,73 +11,43 @@ from . import cli
 
 
 # Dictionary of loaded extension modules indexed by name.
-_loaded = {}
+loaded = {}
 
 
-# Load any Python modules found in the extensions directories.
+# Load Python modules and packages from the extensions directories.
 def load():
 
     # List of directories to search for extensions.
     extdirs = []
 
-    # Default extensions bundled with Ark.
+    # 1. Default extensions bundled with Ark.
     extdirs.append(os.path.join(os.path.dirname(__file__), 'ext'))
 
-    # Global extensions directory/directories.
+    # 2. Global extensions directory (or directories).
     if os.getenv('ARK_EXT') and not cli.parser['no-global-ext']:
         for path in os.getenv('ARK_EXT').split(os.pathsep):
             if os.path.isdir(path):
                 extdirs.append(path)
 
-    # Site extensions.
-    if os.path.isdir(site.ext()) and not cli.parser['no-site-ext']:
-        extdirs.append(site.ext())
-
-    # Theme extensions.
+    # 3. Theme extensions directory.
     if os.path.isdir(site.theme('code')) and not cli.parser['no-theme-ext']:
         extdirs.append(site.theme('code'))
 
-    # Load extensions. This gets a little messy because we're allowing for
-    # 'dev-friendly' plugin loading. An item in an extension directory can
-    # be a python module or package *or* a directory containing a module or
-    # package along with its readme, license file, etc.
+    # 4. Site extensions directory.
+    if os.path.isdir(site.ext()) and not cli.parser['no-site-ext']:
+        extdirs.append(site.ext())
+
+    # Load extensions.
     for extdir in extdirs:
         for item in os.listdir(extdir):
-            itembase = os.path.splitext(item)[0]
-            itemslug = itembase.replace('-', '_')
-            itempath = os.path.join(extdir, item)
-
-            # Skip hidden/packaging files.
-            if item[0] in '_.' or itembase == 'readme':
+            if item.startswith('.'):
                 continue
-
-            # Is the item a directory?
-            if os.path.isdir(itempath):
-
-                # Does the item directory contain an extension package?
-                if os.path.isdir(os.path.join(itempath, itemslug)):
-                    _load(itempath, itemslug)
-
-                # Does the item directory contain an extension module?
-                elif os.path.isfile(os.path.join(itempath, itemslug + '.py')):
-                    _load(itempath, itemslug)
-
-                # The item directory itself must be the extension package.
-                else:
-                    _load(extdir, item)
-
-            # The item is a file. Assume it's a Python module.
-            else:
-                _load(extdir, itembase)
+            itembase = os.path.splitext(item)[0]
+            load_module(extdir, itembase)
 
 
 # Load the named module from the specified directory.
-def _load(directory, module):
-    sys.path.insert(0, directory)
-    _loaded[module] = importlib.import_module(module)
+def load_module(dirpath, modname):
+    sys.path.insert(0, dirpath)
+    loaded[modname] = importlib.import_module(modname)
     sys.path.pop(0)
-
-
-# Return the dictionary of loaded extension modules.
-def loaded():
-    return _loaded
