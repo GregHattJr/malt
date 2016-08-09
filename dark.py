29a6df99c9f2aba312cbe737fe40f74a503b689e
Darren Mulholland <darren@mulholland.xyz>
2016-08-09 01:02:37 +0100

    Housekeeping

--- dark.py
+++ dark.py
@@ -3,13 +3,13 @@
 # Ark: a static website generator in Python.
 #
 # This script acts as the entry point for the development version of Ark; in
-# general use, installing via pip will generate a custom entry point named
+# general use installing via pip will generate a custom entry point named
 # 'ark' on the user's PATH.
 #
 # This script is named 'dark.py' partly because it runs the [d]evelopment
-# version of Ark, but mostly because calling it 'ark.py' can lead to the
+# version of Ark, but mostly because naming it 'ark.py' can lead to the
 # application mistaking it for a site configuration file and entering an
-# infinite self-importing spiral of death...
+# infinite self-importing spiral of death.
 #
 # Author: Darren Mulholland <darren@mulholland.xyz>
 # License: Public Domain
